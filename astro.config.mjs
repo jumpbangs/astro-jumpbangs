617fd922ba7f46c68eb74451b9beb59943844e0f
amitchongbang <achongbang@elementengineering.com.au>
2025-02-19 15:19:09 +0800

    [main]: Downgrade astro from 5.3 to 5.1.7 due to vercel deployment issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
 import remarkCollapse from 'remark-collapse';
@@ -10,8 +9,6 @@ import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
   integrations: [tailwind(), sitemap(), react(), compress(), mdx()],
   markdown: {
     remarkPlugins: [
