b4347ea6cda2e978a3383466242c62d02a3db18b
amitchongbang <achongbang@elementengineering.com.au>
2025-02-19 15:14:12 +0800

    [main]: Fixed astro.config.mjs for deploy failure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 import compress from 'astro-compress';
 import remarkCollapse from 'remark-collapse';
@@ -9,6 +10,8 @@ import remarkToc from 'remark-toc';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind(), sitemap(), react(), compress(), mdx()],
   markdown: {
     remarkPlugins: [
